bd057766cabb2908c05ca6bffb3bdae6ff678a55
Igor Jozefowicz <igorjozefowicz@gmail.com>
2022-09-22 13:44:49 +0200

    create home.index, user.index, department.index routes
    - turn off registration for users - ony admins can create a new user

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])
+    ->name('home.index');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/uzytkownicy', [UserController::class, 'index'])
+    ->name('user.index');
 
-Auth::routes();
+Route::get('/dzial', [DepartmentController::class, 'index'])
+    ->name('department.index');
+
+Auth::routes([
+    'register' => false,
+]);
