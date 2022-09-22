d460a12b4ae2e94ee67aff2bedad8df987f50453
Igor Jozefowicz <igorjozefowicz@gmail.com>
2022-09-22 21:39:08 +0200

    develop HomeController & home view
    - update .gitignore

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+
+        return view(
+            'home',
+            [
+                'user' => $user,
+            ]
+        );
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <h1 class="text-black-50">
-            Zalogowano jako <b>{{ $user->name }}</b> o id: <b>{{ $user->id }}</b>
+    <div class="container-fluid panel__text-container">
+
+        <h1 class="text-black panel__welcome-header">
+            Zalogowano jako 
+            <img src="{{ Storage::url($user->image_path) }}" class="panel__user-image" alt="user image">
+            <b>{{ $user->name }} {{ $user->surname }}</b> o id: <b>{{ $user->id }}</b>
         </h1>
 
-        <h2>
+        <h2 class="panel__role-header">
             Twoja rola to: <b>{{ $user->getRoleNames()[0] }}</b>
         </h2>
+
     </div>
 @endsection
