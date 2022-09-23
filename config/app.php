343f656119796dbe93590ee78049402c46f8ec31
Igor Jozefowicz <igorjozefowicz@gmail.com>
2022-09-23 12:46:37 +0200

    install Intervention\Image & register in ServiceProviders

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -210,7 +211,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
